b0df53a05f90596d0c5f5c69c80ce363cbf64ad3
heenasavaliya <heenasavaliya331@gmail.com>
2024-08-01 14:24:50 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Localstorage from './componets/Localstorage';
+
+
+function App() {
+  return (
+   <>
+   
+ <Localstorage/>
+   
+   </>
+  );
+}
+
+export default App;
